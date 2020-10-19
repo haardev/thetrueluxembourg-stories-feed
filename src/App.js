a8ffa9ef38fa4fb75b666c56416c49c6a2c3ce56
Milen Gardev <milenford@gmail.com>
2020-10-20 00:02:39 +0200

    Adds post changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ function App() {
             author: 'Milen Gardev',
             countryCode: 'BG',
             tag: 'Custom text weq eq eq',
-            text: 'eqeqeq eeq eq',
+            text: '\n' +
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit mattis felis, nec ullamcorper ex finibus at. Proin ornare ut turpis id aliquam. Etiam ut orci eu lacus eleifend consectetur vitae sit amet odio. Pellentesque odio mauris, egestas eget aliquam vitae, consequat in nisl. Integer vitae arcu ipsum. Maecenas laoreet et justo nec consectetur. Maecenas at dignissim sem. Donec eu dui et augue pellentesque congue eget id felis. Sed tincidunt felis ipsum.\n' +
+                '\n' +
+                'Nam laoreet dui eget ex tristique accumsan. Aenean velit lacus, faucibus vel dolor quis, vulputate dictum est. Nunc id efficitur ex, nec scelerisque quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur lobortis nulla nec ipsum ullamcorper, quis lacinia ante vestibulum. In feugiat massa non enim tempus sodales. Nunc id leo non eros commodo faucibus in eget libero. Pellentesque vitae massa finibus, porta tortor in, gravida turpis. Aliquam at eros vel lectus suscipit hendrerit. Aliquam vehicula purus vel eros auctor ultrices. Proin in nisi sagittis, vestibulum est a, aliquam tellus. Sed in est in nulla tempus posuere.\n' +
+                '\n' +
+                'Donec ut erat efficitur ex placerat ornare vel nec libero. Aliquam erat volutpat. In cursus tellus nibh, vel feugiat erat molestie a. Curabitur eu mauris vel tellus ullamcorper facilisis eget eu nibh. Aliquam ac arcu a ipsum pharetra semper et eget dolor. Nam aliquet pretium odio vel vestibulum. Suspendisse potenti. In non sagittis ipsum. Curabitur rutrum ligula eu ligula consectetur sagittis. Donec vitae purus ex. Praesent condimentum purus a malesuada feugiat. Donec volutpat dignissim nunc, id vulputate felis tincidunt blandit. Vestibulum sagittis libero eget purus pellentesque condimentum.',
             favorite: true
         },
         {
@@ -21,7 +26,12 @@ function App() {
             author: 'Milen Gardev',
             countryCode: 'BG',
             tag: 'Custom text weq eq eq',
-            text: 'eqeqeq eeq eq',
+            text: '\n' +
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis suscipit mattis felis, nec ullamcorper ex finibus at. Proin ornare ut turpis id aliquam. Etiam ut orci eu lacus eleifend consectetur vitae sit amet odio. Pellentesque odio mauris, egestas eget aliquam vitae, consequat in nisl. Integer vitae arcu ipsum. Maecenas laoreet et justo nec consectetur. Maecenas at dignissim sem. Donec eu dui et augue pellentesque congue eget id felis. Sed tincidunt felis ipsum.\n' +
+                '\n' +
+                'Nam laoreet dui eget ex tristique accumsan. Aenean velit lacus, faucibus vel dolor quis, vulputate dictum est. Nunc id efficitur ex, nec scelerisque quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur lobortis nulla nec ipsum ullamcorper, quis lacinia ante vestibulum. In feugiat massa non enim tempus sodales. Nunc id leo non eros commodo faucibus in eget libero. Pellentesque vitae massa finibus, porta tortor in, gravida turpis. Aliquam at eros vel lectus suscipit hendrerit. Aliquam vehicula purus vel eros auctor ultrices. Proin in nisi sagittis, vestibulum est a, aliquam tellus. Sed in est in nulla tempus posuere.\n' +
+                '\n' +
+                'Donec ut erat efficitur ex placerat ornare vel nec libero. Aliquam erat volutpat. In cursus tellus nibh, vel feugiat erat molestie a. Curabitur eu mauris vel tellus ullamcorper facilisis eget eu nibh. Aliquam ac arcu a ipsum pharetra semper et eget dolor. Nam aliquet pretium odio vel vestibulum. Suspendisse potenti. In non sagittis ipsum. Curabitur rutrum ligula eu ligula consectetur sagittis. Donec vitae purus ex. Praesent condimentum purus a malesuada feugiat. Donec volutpat dignissim nunc, id vulputate felis tincidunt blandit. Vestibulum sagittis libero eget purus pellentesque condimentum.',
             favorite: true
         },
         {
@@ -52,7 +62,7 @@ function App() {
                 <hr/>
                 <div className="content-container">
                     <div className="post-container">
-                        { data.map((item) => <Post { ...item } />) }
+                        { data.map((item,index) => <Post key={index} { ...item } />) }
                     </div>
                     <div className="post-filter">
                         <div>
--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Post = ({ coverImage, author, tag, countryCode, title, text }) => {
-    const isExpanded = useState(false);
+    const [isExpanded, setExpanded] = useState(false);
+    const [isFavorite, setFavorite] = useState(false);
+    const textRef = useRef(null);
+
+    const renderTrimString = useCallback((inputText) => {
+        let res = !isExpanded ? inputText.substring(0, 490) : inputText;
+        if(res[res.length] === ' ') {
+            res = res.substring(0, res.length - 1);
+        }
+
+        return (
+            <div>{ res }
+                { !isExpanded && (
+                    <span>
+                    <button className="post__read-more__button"
+                            onClick={ () => setExpanded(!isExpanded) }>... more
+                    </button>
+                </span>
+                ) }
+            </div>
+        );
+    }, [textRef, isExpanded, text]);
+
     return (
         <div className="post">
             <div className="post__header">
@@ -17,7 +39,10 @@ const Post = ({ coverImage, author, tag, countryCode, title, text }) => {
                 { title }
             </div>
             <div className="post__text">
-                { text }
+                <div ref={ textRef }
+                     className={ `post__text-content ${ isExpanded === true ? 'post__text-content__expanded' : '' }` }>
+                    { renderTrimString(text) }
+                </div>
             </div>
             <div className="post__read-more">
                 <i className="fa fa-heart" aria-hidden="true"></i>
