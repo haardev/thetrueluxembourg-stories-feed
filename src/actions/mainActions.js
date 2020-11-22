07b0dcd9a021c8eb515a4fce6874dada1c7d8fc7
Milen Gardev <milenford@gmail.com>
2020-11-22 13:55:51 +0100

    Formatting

--- src/actions/mainActions.js
+++ src/actions/mainActions.js
@@ -4,10 +4,10 @@ import {
     FETCH_CATEGORIES_SUCCESS,
     FETCH_POSTS_ERROR,
     FETCH_POSTS_REQUEST,
-    FETCH_POSTS_SUCCESS, FILTER_POSTS
+    FETCH_POSTS_SUCCESS,
+    FILTER_POSTS
 } from '../reducers/actionTypes';
 import { fetchCategories, fetchPosts } from '../apiUtils';
-
 
 const getPosts = async (dispatch) => {
     try {
@@ -44,7 +44,7 @@ const getCategories = async (dispatch) => {
 };
 
 const filterPosts = (dispatch, category) => {
-    //TODO: Maybe it should do again another API call in the future. but for 100 posts its should be fine
+    //TODO: Maybe it should do again another API call in the future but for 100 posts it's should be fine.
     dispatch({
         type: FILTER_POSTS,
         payload: category
