2a7429f777dabbbd16cc1acd45ee125760aee38c
Milen Gardev <milen@haardev.com>
2021-03-20 14:24:41 +0100

    Adds linaria

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import './App.css';
 import FilterBar from './FilterBar';
 import { DataContext } from './DataProvider';
 import PostsContainer from './PostsContainer';
+import { css } from '@linaria/core';
 
-function App() {
+const App = () => {
     const { action } = useContext(DataContext);
+    const containerRoot = css`
+      --main-bg-color: #dadce0;
+      --hover-bg-color: #e8eaec;
+
+      max-width: 950px;
+      margin: 0 auto;
+      padding-top: 18px;
+      width: 100%;
+    `;
 
     useEffect(() => {
         action.getCategories();
         action.getPosts();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div className="App">
-            <div className="container">
+            <div className={containerRoot}>
                 <div className="content-container">
                     <PostsContainer/>
                     <FilterBar/>
@@ -23,6 +32,6 @@ function App() {
             </div>
         </div>
     );
-}
+};
 
 export default App;
--- src/FilterBar.js
+++ src/FilterBar.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { styled } from '@linaria/react';
 import SearchBar from './SearchBar';
+
+const FilterContainer = styled.div`
+  width: 300px;
+  margin-left: 30px;
+  position: sticky;
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const PostFilterWrapper = styled.div`
+  border: 1px solid var(--main-bg-color);
+`;
 
 const FilterBar = () => {
     return (
-        <div className="post-filter">
-            <div className="post-filter__wrapper">
+        <FilterContainer>
+            <PostFilterWrapper>
                 <SearchBar/>
-            </div>
-        </div>
+            </PostFilterWrapper>
+        </FilterContainer>
     );
 };
 
